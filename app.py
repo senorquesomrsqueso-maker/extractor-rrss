413cf78290e51af134e336a82a3c0e3c7e64c019
senorquesomrsqueso-maker <senorquesomrsqueso@gmail.com>
2026-01-20 13:57:08 -0400

    app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from io import BytesIO
 DRIVE_API_KEY = "AIzaSyBjETNqerBHpqCBQBH7B1bZl55eYWrtMQk"
 
 st.set_page_config(
-    page_title="AUDIT-ELITE PRO V29 - OMNI TITAN EXPANDED",
+    page_title="BS LATAM",
     page_icon="🛡️",
     layout="wide",
     initial_sidebar_state="expanded"
